f210a28ef5b3d595ffaaa32f623e88a043c7ba94
SpeckyYT <Specky.fans@gmail.com>
2020-12-19 21:32:54 +0100

    0.0.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# Neapolitan-wrapper
+
+The first italian-to-neapolitan translator for Node.js!
+
+## Installation
+
+```
+npm i neapolitan-wrapper --save
+```
+
+## Usage
+
+```js
+const neapolitan = require('neapolitan-wrapper');
+
+neapolitan("Bella a tutti ragazzi, sto usando Node.js per tradurre questa stringa italiana di testo!")
+.then(result => {
+    console.log(result);
+})
+```
+
+# Credits
+
+Inspired by [Lory-git](https://github.com/Lory-git/neapolitan-wrapper)
---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+const removeaccents = require('remove-accents');
+
+const baseURL = 'http://www.giggino.com/traduzione-italiano-napoletano.asp?t='
+
+/**
+ * 
+ * @param {string} input
+ * @returns {Promise<string>}
+ */
+async function translater(input){
+    if(typeof input != 'string') return '';
+
+    return fetch(baseURL + encodeURI(removeaccents(input)))
+    .then(res => res.text())
+    .then(res => res.replace(/[\n\r]/g,''))
+    .then(res => res.match(/(?<=\<td\>\<span class="Stile3"\>)(.*)(?=\<\/span\>\<\/td\>)/g)[0])
+    .then(res => res.replace(/�/g,'').replace(/\s+/g,' ').trim())
+}
+
+module.exports = translater;
