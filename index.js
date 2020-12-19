9228ef1b0f7351aa5984b744d4b7c82d77a6c4ce
SpeckyYT <Specky.fans@gmail.com>
2020-12-19 22:31:48 +0100

    forgot to include new line and space on input

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const baseURL = 'http://www.giggino.com/traduzione-italiano-napoletano.asp?t='
 async function translate(input){
     if(typeof input != 'string') return '';
 
-    return fetch(baseURL + removeaccents(input).replace(/[^a-zA-Z0-9]+/g,' '))
+    return fetch(baseURL + removeaccents(input).replace(/[^a-zA-Z0-9\n\s]+/g,' '))
     .then(res => res.text())
     .then(res => res.replace(/[\n\r]/g,''))
     .then(res => res.match(/(?<=<td><span class="Stile3">)(.*)(?=<\/span><\/td>)/g)[0])
