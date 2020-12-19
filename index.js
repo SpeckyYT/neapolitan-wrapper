8ed231166942dc1dc6fb1fce6b9de33885de3bc1
SpeckyYT <Specky.fans@gmail.com>
2020-12-19 23:40:35 +0100

    replaced \s with spaces

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ const baseURL = 'http://www.giggino.com/traduzione-italiano-napoletano.asp?t='
 async function translate(input){
     if(typeof input != 'string') return '';
 
-    return fetch(baseURL + encodeURI(removeaccents(input).replace(/[^a-zA-Z0-9\n\s]+/g,' ')))
+    return fetch(
+        baseURL +
+        encodeURI(
+            removeaccents(input)
+            .replace(/[^a-zA-Z0-9\n ]+/g, ' ')
+        )
+    )
     .then(res => res.text())
     .then(res => res.replace(/[\n\r]/g,''))
     .then(res => res.match(/(?<=<td><span class="Stile3">)(.*)(?=<\/span><\/td>)/g)[0])
     .then(res => 
         res
-        .replace(/�/g,'')
         .replace(/<br>/g,'\n')
-        .replace(/\s+/g,' ')
-        .replace(/^\s+|\s+$/gm,'')
+        .replace(/�/g,'')
+        .replace(/ +/g,' ')
+        .replace(/^ +| +$/gm,'')
     );
 }
 
